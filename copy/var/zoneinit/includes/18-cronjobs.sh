693d87c47d6eef7ede4fccd36a27fa072514e091
jfqd <jfqd@blun.org>
2018-02-03 17:49:35 +0100

    add cronjob for munin data backup

---
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -0,0 +1,7 @@
+# create cronjob for ssl-expire script
+CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
+15 3 * * 6 /opt/local/sbin/pkg_admin audit
+15 * * * * /opt/qutic/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
+8 * * * * [[ -x /data/bin/backup ]] && /data/bin/backup 1>/dev/null
+'
+(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
