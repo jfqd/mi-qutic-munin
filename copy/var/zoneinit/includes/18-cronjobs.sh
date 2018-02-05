51695fa8c88e635c6df18e4e19856305f5cdadd1
jfqd <jfqd@blun.org>
2018-02-05 15:28:25 +0100

    skip stderr too

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -2,6 +2,6 @@
 CRON='0 9 * * 1 /opt/qutic/bin/ssl-expire.sh
 15 3 * * 6 /opt/local/sbin/pkg_admin audit
 15 * * * * /opt/qutic/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
-8 * * * * [[ -x /data/bin/backup ]] && /data/bin/backup 1>/dev/null
+8 * * * * [[ -x /data/bin/backup ]] && /data/bin/backup 2>&1 1>/dev/null
 '
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
